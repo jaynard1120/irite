92d544c7b2dd79d89c03d2fa32a13411a7eb8d1d
Mitchven <malyn.gamboa@student.passerellesnumeriques.org>
2021-01-29 13:18:53 +0800

    Changes

--- src/app/Components/Auth/register/register.component.ts
+++ src/app/Components/Auth/register/register.component.ts
@@ -1,5 +1,4 @@
 import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
-import { catchError } from 'rxjs/operators';
 import { ApiService } from './../../../Services/api.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -16,39 +15,42 @@ export class RegisterComponent implements OnInit {
     private service: ApiService,
     private fb: FormBuilder
   ) { }
+  // check:Boolean = false;
   userAccount: any;
   ngOnInit(): void {
     this.userAccount = this.fb.group({
       username: ['',[Validators.required,Validators.minLength(5),Validators.maxLength(8)]],
       dateOfBirth: ['',Validators.required],
       email: ['',[Validators.required,Validators.email]],
+      check:['',Validators.required],
       password: ['',[Validators.required,Validators.minLength(8),Validators.maxLength(12)]],
       password_confirmation: ['',[Validators.required,,Validators.minLength(8),Validators.maxLength(12)]]
     },{validator: this.confirmation('password','password_confirmation')})
   }
   icon = true;
   button = "Register"
+  unique = "";
   register(userData:any){
     this.icon = false;
     this.button = "Signing up..."
     this.service.register(userData).subscribe(res => {
       this.router.navigate(['login'])
+    },error => {
+      console.log(error)
+      alert("Account already exist!")
+      location.reload()
     }
     )
     
   }
-
-  //Validation
   confirmation(controlName: string, matchingControlName: string){
     return (formGroup: FormGroup) => {
       const control = formGroup.controls[controlName];
       const matchingControl = formGroup.controls[matchingControlName];
   
       if (matchingControl.errors && !matchingControl.errors.mustMatch) {
-        // return if another validator has already found an error on the matchingControl
         return;
       }
-      // set error on matchingControl if validation fails
       if (control.value !== matchingControl.value) {
         matchingControl.setErrors({ mustMatch: true });
       } else {
